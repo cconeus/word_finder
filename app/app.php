8f158014807d004b77d3d37cbae4d583904bb0c3
Dana <cconeus@gmail.com>
2015-08-14 12:30:11 -0700

    view files created

---
+++ app/app.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array (
+        'twig.path' =>__DIR__.'/../views'
+      ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->post("/results_page", function() use ($app) {
+        $task = new RepeatCounter($_GET['usr_string'], $_GET['word_search']);
+        return $app['twig']->render('results.html.twig', array('usr_word' => $usr_word));
+    });
+
+    return $app;
+?>
--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,15 +1,17 @@
 <?php
 
+
     class RepeatCounter
     {
 
         function countRepeats($usr_word, $usr_string)
         {
 
-            $usr_word = strtoupper("cat");
-            $usr_string = strtoupper("The cat in the hat");
+            $usr_word = strtoupper($_GET['word_search']);
+            $usr_string = strtoupper($_GET['usr_string']);
 
             $output = substr_count($usr_string, $usr_word);
+
             return $output;
         }
     }
